e4d0b55c82454811d599ef0476f7aa31f85e8b15
Mateusz Nikiel <pascal.nikiel@gmail.com>
2020-11-25 12:06:03 +0100

    Endpoint for changing peer ip or proxy ip

--- wireguard-peer/root/etc/services.d/api/app.py
+++ wireguard-peer/root/etc/services.d/api/app.py
@@ -1,12 +1,12 @@
 import json
 import logging
-import subprocess
-import signal
-import sys
 import os
+import subprocess
+import time
+from threading import Thread
 
 import requests
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -38,37 +38,48 @@ def get_config(ip, proxy_ip, private_key, public_key):
     'Endpoint = {}:51820\n' \
     'AllowedIPs = 0.0.0.0/0\n'.format(ip, private_key, public_key, proxy_ip)
 
-options_file = open('/data/options.json', 'r')
-options = json.load(options_file)
-options_file.close()
-
-ip = get_ip(options['peer_id'])
-proxy_ip = get_proxy_ip(options['peer_id'])
-private_key = options['private_key']
-public_key = os.getenv('PUBLIC_KEY')
-logger.info('IP: {}, proxy IP: {}'.format(ip, proxy_ip))
-
-@app.route('/healthcheck')
-def healthcheck():
-  new_ip = get_ip(options['peer_id'])
-  new_proxy_ip = get_proxy_ip(options['peer_id'])
-
-  if new_ip != ip or new_proxy_ip != proxy_ip:
-    generate_config(new_ip, new_proxy_ip, private_key, public_key)
-    logger.info('New IP: {}, new proxy IP: {}'.format(ip, proxy_ip))
-
-    subprocess.run(['wg-quick', 'down', 'wg0'])
-    subprocess.run(['wg-quick', 'up', 'wg0'])
-
-  return '', 200
-
 def generate_config(ip, proxy_ip, private_key, public_key):
   config = get_config(ip, proxy_ip, private_key, public_key)
   config_file = open('/config/wg0.conf', 'w')
   config_file.write(config)
   config_file.close()
 
+@app.route('/peer', methods=['PATCH'])
+def update_peer():
+  new_ip = ip
+  new_proxy_ip = proxy_ip
+  if 'peer_ip' in request.json:
+    logger.info('Peer IP changes: {}'.format(request.json['peer_ip']))
+    new_ip = request.json['peer_ip']
+  if 'proxy_ip' in request.json:
+    new_proxy_ip = request.json['proxy_ip']
+
+  generate_config(new_ip, new_proxy_ip, private_key, public_key)
+  logger.info('New IP: {}, new proxy IP: {}'.format(new_ip, new_proxy_ip))
+
+  def restart_wireguard():
+    time.sleep(1)
+    logger.info('Restarting wirequard interface')
+    subprocess.run(['wg-quick', 'down', 'wg0'])
+    subprocess.run(['wg-quick', 'up', 'wg0'])
+
+  thread = Thread(target=restart_wireguard)
+  thread.start()
+
+  return '', 200
+
 if __name__ == '__main__':
+  options_file = open('/data/options.json', 'r')
+  options = json.load(options_file)
+  options_file.close()
+
+  ip = get_ip(options['peer_id'])
+  proxy_ip = get_proxy_ip(options['peer_id'])
+  private_key = options['private_key']
+  public_key = os.getenv('PUBLIC_KEY')
+  logger.info('IP: {}, proxy IP: {}'.format(ip, proxy_ip))
+
   generate_config(ip, proxy_ip, private_key, public_key)
   subprocess.run(['wg-quick', 'up', 'wg0'])
+    
   app.run(debug=True, host='0.0.0.0')
