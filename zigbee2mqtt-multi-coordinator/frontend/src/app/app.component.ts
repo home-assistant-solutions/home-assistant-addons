1312a6c1adf2a31ca162647f4c8fbada09ef22b2
Mateusz Nikiel <pascal.nikiel@gmail.com>
2021-06-17 10:16:54 -0400

    feat: ability to select coordinator in frontend

--- zigbee2mqtt-multi-coordinator/frontend/src/app/app.component.ts
+++ zigbee2mqtt-multi-coordinator/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  options: any = {};
+  selectedCoordinator = null;
+  frontendUrl: any;
+
+  constructor(private dataService: DataService, private sanitizer: DomSanitizer) {
+    this.dataService.getOptions().subscribe(options => {
+      this.options = options;
+    })
+  }
+
+  onCoordinatorSelected(coordinator: any) {
+    this.frontendUrl = this.sanitizer.bypassSecurityTrustResourceUrl('http://192.168.157.2:8080/zigbee2mqtt-' + coordinator);
+    this.selectedCoordinator = coordinator;
+  }
 }
