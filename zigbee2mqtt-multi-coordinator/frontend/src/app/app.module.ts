f5c7fd5e98badafc5615074487dc22acb30041e2
Mateusz Nikiel <pascal.nikiel@gmail.com>
2021-06-17 10:16:54 -0400

    feat: ability to select coordinator in frontend

--- zigbee2mqtt-multi-coordinator/frontend/src/app/app.module.ts
+++ zigbee2mqtt-multi-coordinator/frontend/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
